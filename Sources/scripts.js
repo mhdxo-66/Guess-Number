b7f07ace6b7e4c2b05625aeb666c1e0a15a5bac0
mhdxo-66 <mahdiarvandi99@gmail.com>
2026-08-10 22:28:05 +0330

    Added Score System

--- Sources/scripts.js
+++ Sources/scripts.js
@@ -12,11 +12,21 @@ document.querySelector('.form').addEventListener('submit', function (e) {
     inpnum = Number(document.querySelector('.numes').value);
     if (inpnum > rnm) {
         document.querySelector('.h2').innerHTML = "To High ++++";
+        document.body.style.backgroundColor = "red";
+        document.querySelector('.score').innerHTML = score;
+        score--;
     } else if (inpnum < rnm) {
         document.querySelector('.h2').innerHTML = "To Low ----";
+        document.body.style.backgroundColor = "blue";
+        document.querySelector('.score').innerHTML = score;
+        score--;
     } else if (inpnum === rnm) {
+        score++;
         document.querySelector('.h2').innerHTML = "🎉 Correct Number!";
         document.body.style.backgroundColor = "#5EAF46";
         document.querySelector('.ans').innerHTML = rnm;
+        document.querySelector('.score').innerHTML = score;
+        highscore++;
+        document.querySelector('.hscore').innerHTML = highscore;
     }
 });

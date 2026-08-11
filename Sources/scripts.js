eaf2c235cac8f43565b256d622ad97aae32268a5
mhdxo-66 <mahdiarvandi99@gmail.com>
2026-08-11 13:03:48 +0330

    Added Refresh Number After Correct

--- Sources/scripts.js
+++ Sources/scripts.js
@@ -2,9 +2,15 @@
 let score = 20;
 let highscore = 0;
 
-const rnm = Math.floor(Math.random() * 20) + 1;
-console.log(rnm)
-
+document.querySelector('.rst').addEventListener('click', function () {
+    localStorage.clear();
+})
+let rnm;
+function rnmc() {
+    return rnm = (Math.floor(Math.random() * 20) + 1);
+};
+rnmc();
+console.log(rnm);
 document.querySelector('.form').addEventListener('submit', function (e) {
     console.log(rnm)
     e.preventDefault();
@@ -13,22 +19,23 @@ document.querySelector('.form').addEventListener('submit', function (e) {
     if (inpnum > rnm) {
         document.querySelector('.h2').innerHTML = "To High ++++";
         document.body.style.backgroundColor = "red";
-        document.querySelector('.score').innerHTML = score;
         score--;
+        document.querySelector('.score').innerHTML = score;
     } else if (inpnum < rnm) {
         document.querySelector('.h2').innerHTML = "To Low ----";
         document.body.style.backgroundColor = "blue";
-        document.querySelector('.score').innerHTML = score;
         score--;
+        document.querySelector('.score').innerHTML = score;
     } else if (inpnum === rnm) {
         if (score < 20) {
             score++;
         }
         document.querySelector('.h2').innerHTML = "🎉 Correct Number!";
-        document.body.style.backgroundColor = "#5EAF46";
+        document.body.style.backgroundColor = "green";
         document.querySelector('.ans').innerHTML = rnm;
         document.querySelector('.score').innerHTML = score;
         highscore++;
         document.querySelector('.hscore').innerHTML = highscore;
+        rnmc();
     }
 });

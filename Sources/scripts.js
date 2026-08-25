a9dd177611a2855ad5534e66f275274af7440507
mhdxo-66 <mahdiarvandi99@gmail.com>
2026-08-25 12:52:56 +0330

    Optimized Code Block

--- Sources/scripts.js
+++ Sources/scripts.js
@@ -19,31 +19,7 @@ document.querySelector('.form').addEventListener('submit', function (e) {
     e.preventDefault();
     let inpnum;
     inpnum = Number(document.querySelector('.numes').value);
-    if (inpnum > rnm) {
-        document.querySelector('.status').textContent = "To High ++++";
-        document.body.style.backgroundColor = "red";
-        if (score > 0) {
-            score--;
-        } else if (score === 0) {
-            alert('Game Over👾');
-            rst();
-        }
-        localStorage.setItem('score', score);
-        lvst();
-
-    } else if (inpnum < rnm) {
-        document.querySelector('.status').textContent = "To Low ----";
-        document.body.style.backgroundColor = "blue";
-        if (score > 0) {
-            score--;
-        } else if (score === 0) {
-            alert('Game Over👾');
-            rst();
-        }
-        localStorage.setItem('score', score);
-        lvst();
-
-    } else if (inpnum === rnm) {
+    if (inpnum === rnm) {
         document.querySelector('.status').textContent = "🎉 Correct Number!";
         document.body.style.backgroundColor = "green";
         document.querySelector('.ans').textContent = rnm;
@@ -53,10 +29,24 @@ document.querySelector('.form').addEventListener('submit', function (e) {
         highscore++;
         localStorage.setItem('highscore', highscore);
         rnmc();
+        localStorage.setItem('score', score);
+        lvst();
+    } else if (inpnum < rnm || inpnum > rnm) {
+        document.querySelector('.status').textContent = inpnum < rnm ? 'To Low ----' : 'To High ++++'
+        document.body.style.backgroundColor = inpnum < rnm ? 'blue' : 'red';
+        if (score >= 0) {
+            score--;
+            if (score === 0) {
+                alert('Game Over 👾');
+                rst();
+            }
+        }
+        lvst();
+        localStorage.setItem('score', score);
     }
-    localStorage.setItem('score', score);
-    lvst();
 });
+
+// 
 function lvst() {
     document.querySelector('.score').textContent = `💯 Score : ${score}`;
     document.querySelector('.hscore').textContent = `🥇 Highscore : ${highscore}`;
